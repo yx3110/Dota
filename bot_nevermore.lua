0c64c772b96aa983fd85100e30f27172534dd981
yx3110 <eclipse.myworld@gmail.com>
2016-12-20 21:18:39 +0900

    Initial commit

---
+++ bot_nevermore.lua
@@ -0,0 +1,61 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: eclipse
+-- Date: 19/12/2016
+-- Time: 10:53
+-- To change this template use File | Settings | File Templates.
+--
+counter = 0;
+
+function Think()
+    if(counter%10~=0)
+        then
+    else
+        features = getFeatures();
+        action = getAction(feature);
+        counter = counter+1;
+    end
+end
+
+function getFeatures()
+-- [[Features:
+    -- enemy creeps around
+    -- Enemy creep is uphill
+    -- enemy hero distance
+    -- enemy hero is uphill
+    -- my hero position
+    -- gold
+    -- enemy hero hp
+    -- enemy hero mana
+    -- my hero hp
+    -- my hero mana
+    -- lvl
+    -- exp to next lvl
+    -- direction facing
+    -- enemy hero around
+    -- time of the game
+    -- item
+
+    --
+    --]]
+
+end
+
+function getAction(feature)
+--Actions
+    --raze short
+    --raze mid
+    --raze far
+    --attack unit
+    --ultimate
+    --move
+    --turn
+    --Use item
+        --1
+        --2
+        --3
+        --4
+        --5
+        --6
+    --
+end
